1d7b3d5e7e79ef38377d48a95c799a504fea10e0
vishal <singhdhamivishal@gmail.com>
2022-01-30 02:04:22 +0530

    covid cases

---
+++ async.js
@@ -0,0 +1,8 @@
+const fs=require("fs");
+console.log("Before");
+//let data=fs.readFileSync("f1.text");
+//console.log("" + data);
+//console.log("After");
+//console.log("Mean While");
+fs.readFile("f1.txt   ")
+
